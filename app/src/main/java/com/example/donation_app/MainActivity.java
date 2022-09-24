3e5246e4c40fd39916da45c016fc5d01ac119d52
ducanh01-11-99 <ducanh20176919@gmail.com>
2022-09-24 22:17:50 +0700

    Create - layout donate activity

--- app/src/main/java/com/example/donation_app/MainActivity.java
+++ app/src/main/java/com/example/donation_app/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.donation_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button donateButton;
+        RadioGroup paymentMethod;
+        ProgressBar progressBar;
+        NumberPicker amountPicker;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        donateButton = (Button) findViewById(R.id.donateButton);
     }
 }
